a3b8685415b5ca2316de69b197c8d7d2057c305d
Ngoc Hoang <hoangcn1998@gmail.com>
2021-06-27 22:18:28 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Menu from './components/Menu';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function onHandleToggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <div className="App">
-
+      <Header onHandleToggleMenu={onHandleToggleMenu} showMenu={showMenu} />
+      <Menu showMenu={showMenu} onHandleToggleMenu={onHandleToggleMenu} />
     </div>
   );
 }
