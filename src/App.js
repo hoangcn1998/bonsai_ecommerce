34bb04f583e143ab2c20bbda4770b49cb5192187
Ngoc Hoang <hoangcn1998@gmail.com>
2021-06-29 22:51:31 +0700

    reset layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Customer/Header';
 import Menu from './components/Menu';
 
 function App() {
