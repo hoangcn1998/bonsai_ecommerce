aadfcd0ebe8bb297fc8c4d03a8704be28c073feb
Ngoc Hoang <hoangcn1998@gmail.com>
2021-07-10 21:01:46 +0700

    Default login

--- src/components/Customer/Header/Modal/register/FormLogin.jsx
+++ src/components/Customer/Header/Modal/register/FormLogin.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import axios from "axios";
-import jwt_decode from "jwt-decode";
 import { useForm } from "react-hook-form";
 
 const FormLogin = () => {
@@ -8,32 +6,16 @@ const FormLogin = () => {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-    axios
-      .post("http://localhost:3000/api/signin", data)
-      .then((res) => {
-        const accessToken = res.data.accessToken;
-        const user = jwt_decode(accessToken);
-        console.log(user);
-        axios
-          .get(`http://localhost:3000/api/users/${user.sub}`)
-          .then((res) => {
-            const user = res.data;
-            console.log(`get user by id`, user);
-            console.log("user role", user.role);
-          })
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => console.log(err));
-    reset({ example: "", exampleRequired: "" });
-  };
+  function onSubmitLogin(e) {
+    e.preventDefault();
+
+    alert("asdf");
+  }
 
   return (
-    <form className="form form__login" onSubmit={handleSubmit(onSubmit)}>
+    <form className="form form__login" onSubmit={handleSubmit(onSubmitLogin)}>
       <input
         type="text"
         placeholder="Email"
