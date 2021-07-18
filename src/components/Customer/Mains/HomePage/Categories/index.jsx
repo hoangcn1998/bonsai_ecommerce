4130c1a233893ac81582367a73edd1ff77ce624d
Ngoc Hoang <hoangcn1998@gmail.com>
2021-07-18 20:53:29 +0700

    fix-bugs

--- src/components/Customer/Mains/HomePage/Categories/index.jsx
+++ src/components/Customer/Mains/HomePage/Categories/index.jsx
@@ -4,9 +4,9 @@ import "./style.scss";
 
 function Category(props) {
   const [categories] = useState([
-    {id: 1, name: "Table Tree Plant", count: 50},
-    {id: 2, name: "Indoor Plants", count: 75},
-    {id: 3, name: "House Plants", count: 50},
+    { id: 1, name: "Table Tree Plant", count: 50 },
+    { id: 2, name: "Indoor Plants", count: 75 },
+    { id: 3, name: "House Plants", count: 50 },
   ])
   return (
     <div className="category">
